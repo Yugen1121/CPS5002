a6b8a5568afaf84962db2a6e5e681bdb98b03640
Yugen1121 <yugenlimbu777@gmail.com>
2025-11-04 14:24:32 +0000

    removed some redundent lines

--- week5/main.py
+++ week5/main.py
@@ -5,35 +5,33 @@ from models.Shark import Shark
 class Main():
     def __init__(self):
         self.State = [[0 for i in range(20)] for i in range(20)]
-        self.IniPlanktonNumber = random.randint(12, 16)
-        self.IniShardineNumber = random.randint(6,8)
-        self.IniSharkNumber = random.randint(2, 5)
         self.Time = 0
+        self.initialiseObjects()
     
     def initialiseObjects(self):
         # initialise plankton
-        for i in range(self.IniPlanktonNumber):
+        for i in range(random.randint(12, 16)):
             while True:
                 x = random.randint(0,20)
                 y = random.randint(0,20)
                 if self.State[x][y] == 0:
-                    self.State[x][y] == Plankton()
+                    self.State[x][y] = Plankton()
                     break
         # initialise Shardine
-        for i in range(self.IniShardineNumber):
+        for i in range(random.randint(6,8)):
             while True:
                 x = random.randint(0,20)
                 y = random.randint(0,20)
                 if self.State[x][y] == 0:
-                    self.State[x][y] == Shardine()
+                    self.State[x][y] = Shardine()
                     break
         # initialise Shark
-        for i in range(self.IniSharkNumber):
+        for i in range(random.randint(2, 5)):
             while True:
                 x = random.randint(0,20)
                 y = random.randint(0,20)
                 if self.State[x][y] == 0:
-                    self.State[x][y] == Shark()
+                    self.State[x][y] = Shark()
                     break
 
-    
+
